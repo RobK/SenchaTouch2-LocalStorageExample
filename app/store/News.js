e697ae2a1b87058c4adb17531717ece353b68a7e
Robert Kehoe <robert.kehoe@f-secure.com>
2012-11-22 22:27:40 +0800

    Updating URL and making comments clearer

--- app/store/News.js
+++ app/store/News.js
@@ -15,7 +15,7 @@ Ext.define('default.store.News', {
     proxy: {
       timeout: 3000, // How long to wait before going into "Offline" mode, in milliseconds.
       type: 'ajax',
-      url: 'http://slashdot.org/data/news.json'
+      url: 'http://example.org/data/news.json' // Sample URL that simulates offline mode. Example.org does not allow cross-domain requests so this will always fail
     },
     autoLoad: true
   }
